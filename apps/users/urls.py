d1973732b09a5175b68b7ad3a0b510d636c737f6
glorr12 <igor.fishkov1991@gmail.com>
2026-09-15 12:02:09 +0100

    .

---
+++ apps/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from apps.users.views import MeView, RegisterView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('me/', MeView.as_view(), name='me'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ apps/users/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics, permissions
+
+from apps.users.models import User
+from apps.users.serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
